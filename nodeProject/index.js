474e704c104a8cee071c5f462e4b03f793b3407d
Quentin Couderc <quentin.couderc@socotec.com>
2023-04-05 11:58:20 +0200

    fix(files + body-parser): change name of files + refacto + install body-parser and create express webserver

--- nodeProject/index.js
+++ nodeProject/index.js
@@ -1,6 +1,19 @@
-import http from "http";
-import routes from "./routes.js"
+import express from "express";
 
-const webServer = http.createServer(routes.handler)
+const app = express();
 
-webServer.listen(3000);
+app.use((req, res, next) => {
+  console.log("je suis dans un middleware")
+  next()
+});
+
+app.use((req, res, next) => {
+  console.log("je suis dans un autre middleware")
+  next()
+});
+
+app.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+app.listen("3000");
--- sandBox/webserver_basic_avec_module.js
+++ sandBox/webserver_basic_avec_module.js
@@ -1,45 +1,6 @@
 import http from "http";
-import fs from "fs";
-import { printMessage as printMessageFromModule} from "./module.js";
-import someUtilsFunction from "./module.js"
+import routes from "./routes_module_webserver_avec_module.js"
 
-const webServer = http.createServer((request, response) => {
-  const url = request.url;
-  const method = request.method;
-  if(url === "/") {
-    response.write(`
-      <html>
-        <head>
-          <title>WebServer Title</title>
-        </head>
-        <body>
-          <form action="message" method="post">
-            <label for="message">Enter your message: </label>
-            <br/>
-            <input type="text" name="message" id="message" required>
-            <br/>
-            <input type="submit" value="Send">
-          </form>
-        </body>
-      </html>
-    `);
-    return response.end()
-  }
-  if(url === "/message" && method === "POST") {
-    const body = [];
-    request.on("data", (chunk) => {
-      body.push(chunk)
-    })
-    request.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split("=")[1]
-      fs.writeFileSync("message.txt", message)
-      printMessageFromModule(message);
-    })
-    response.statusCode = 302;
-    response.setHeader("Location", "/")
-    return response.end()
-  }
-})
+const webServer = http.createServer(routes.handler)
 
 webServer.listen(3000);
